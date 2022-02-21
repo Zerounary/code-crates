60d6dd215316e50087836cd32b828ab133dd226e
Zerounary <850657663@qq.com>
2022-02-21 14:31:35 +0800

    java js 改为 ts

--- src/common/java.ts
+++ src/common/java.ts
@@ -13,9 +13,11 @@ export let transfrom = (text = '') => {
   let sql_reg = /==>\s+Preparing: .*\r?\n/gm;
   let params_reg = /==>\s+Parameters: .*\r?\n/gm;
   try {
-    let sql = trim(trimStart(sql_reg.exec(text)[0] || '', '==>  Preparing: '));
+    let found_sql = sql_reg.exec(text) || [];
+    let found_param = params_reg.exec(text) || [];
+    let sql = trim(trimStart(found_sql[0] || '', '==>  Preparing: '));
     let params = trim(
-      trimStart(params_reg.exec(text)[0] || '', '==>  Parameters: '),
+      trimStart(found_param[0] || '', '==>  Parameters: '),
     );
     for (let param of split(params, ', ')) {
       let parts = split(param, '(');
@@ -37,7 +39,9 @@ export let transfrom = (text = '') => {
 };
 
 
-export let errorTips = `错误格式！
+export let errorTips = `
+错误格式！
+
 如以下日志：
 
           G 2408 --- [http-nio-8080-exec-1] SELECT.527967772                         : ==>  Preparing: SELECT * FROM ( SELECT rownum as "#NO", "#O".* FROM (WITH v_w as (SELECT sum(i.tot_amt_mark) as tot_amt_mark FROM c_storemark t, c_storemarkitem i WHERE t.id = i.c_storemark_id AND i.monthdate BETWEEN ? AND ? AND exists (select a1.c_store_id from mb_users_stores a1, c_store s1 where a1.c_store_id = s1.id AND a1.mb_users_id = ? AND a1.c_store_id = t.c_store_id ) AND t.isactive = 'Y') select round(nvl(c."零售总额",0)/10000,2) as "零售总额", c."目标值", round(c."预估值"/10000,2) as "预估值", decode(c."目标值",0,0,round(c."零售总额"/c."目标值",2)*100) as "达成率", decode(c."目标值",0,0,round(c."预估值"/c."目标值",2)*100) as "预估达成率", nvl((SELECT round(sum(t.TOT_AMT_ACTUAL)/10000, 2) FROM b_vipmoney t WHERE t.status = 2 AND exists (select a1.c_store_id from mb_users_stores a1, c_store s1 where a1.c_store_id = s1.id AND a1.mb_users_id = ? AND a1.c_store_id = t.c_store_id ) and t.billdate between ? and ?),0) as "储值" from (SELECT b.tot_amt_actual as "零售总额", (nvl(b.tot_amt_actual,0) / (to_date(to_char(sysdate, 'yyyymmdd'),'yyyymmdd') - to_date(?,'yyyymmdd') + 1)) * (to_date(?,'yyyymmdd') - to_date(?,'yyyymmdd') + 1) as "预估值", nvl((select tot_amt_mark from v_w),0) as "目标值" FROM (SELECT SUM(i.RETAIL_AMT) AS tot_amt_actual FROM m_retail t, m_retailitem i, mbv_retail_product p WHERE t.id = i.m_retail_id AND i.m_product_id = p.id AND t.isactive = 'Y' AND t.status = 2 AND t.billdate BETWEEN ? AND ? AND exists (select a1.c_store_id from mb_users_stores a1, c_store s1 where a1.c_store_id = s1.id AND a1.mb_users_id = ? AND a1.c_store_id = t.c_store_id ) ) b) c) "#O" WHERE rownum < 30) 
@@ -49,4 +53,4 @@ export let errorTips = `错误格式！
 将被替换为：
 
 SELECT * FROM ( SELECT rownum as "#NO", "#O".* FROM (WITH v_w as (SELECT sum(i.tot_amt_mark) as tot_amt_mark FROM c_storemark t, c_storemarkitem i WHERE t.id = i.c_storemark_id AND i.monthdate BETWEEN 20220107 AND 20220107 AND exists (select a1.c_store_id from mb_users_stores a1, c_store s1 where a1.c_store_id = s1.id AND a1.mb_users_id = 87 AND a1.c_store_id = t.c_store_id ) AND t.isactive = 'Y') select round(nvl(c."零售总额",0)/10000,2) as "零售总额", c."目标值", round(c."预估值"/10000,2) as "预估值", decode(c."目标值",0,0,round(c."零售总额"/c."目标值",2)*100) as "达成率", decode(c."目标值",0,0,round(c."预估值"/c."目标值",2)*100) as "预估达成率", nvl((SELECT round(sum(t.TOT_AMT_ACTUAL)/10000, 2) FROM b_vipmoney t WHERE t.status = 2 AND exists (select a1.c_store_id from mb_users_stores a1, c_store s1 where a1.c_store_id = s1.id AND a1.mb_users_id = 87 AND a1.c_store_id = t.c_store_id ) and t.billdate between 20220107 and 20220107),0) as "储值" from (SELECT b.tot_amt_actual as "零售总额", (nvl(b.tot_amt_actual,0) / (to_date(to_char(sysdate, 'yyyymmdd'),'yyyymmdd') - to_date(20220107,'yyyymmdd') + 1)) * (to_date(20220107,'yyyymmdd') - to_date(20220107,'yyyymmdd') + 1) as "预估值", nvl((select tot_amt_mark from v_w),0) as "目标值" FROM (SELECT SUM(i.RETAIL_AMT) AS tot_amt_actual FROM m_retail t, m_retailitem i, mbv_retail_product p WHERE t.id = i.m_retail_id AND i.m_product_id = p.id AND t.isactive = 'Y' AND t.status = 2 AND t.billdate BETWEEN 20220107 AND 20220107 AND exists (select a1.c_store_id from mb_users_stores a1, c_store s1 where a1.c_store_id = s1.id AND a1.mb_users_id = 87 AND a1.c_store_id = t.c_store_id ) ) b) c) "#O" WHERE rownum < 30)
-    `
+ `;
