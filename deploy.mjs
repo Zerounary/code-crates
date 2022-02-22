ba0b05c40e28f23c58049f9be56f61257baaf94c
Zerounary <850657663@qq.com>
2022-02-22 11:07:28 +0800

    添加自动部署脚本

---
+++ deploy.mjs
@@ -0,0 +1,24 @@
+#!/usr/bin/env zx
+import 'zx/globals'
+
+// $.verbose = false;
+
+const {green} = chalk;
+
+let host = 'spartan_156';
+let ip = '45.145.75.156';
+const site = 'tools.jianpuwang.cn';
+const remotePath = `/root/${site}/html`;
+
+console.log('删除已有文件');
+await $`ssh ${host} "rm -rf ${remotePath}"`;
+console.log(green('文件删除完成'));
+
+console.log('2秒后,开始复制文件');
+await sleep(2000);
+// "" 中的数据 变量传入，否则会报错
+await $`rsync -avz -e "rsync-ssh -i D:\\Tools\\Commands\\cwrsync\\id_rsa_45.145.75.156" --chmod=ugo=rwX dist/ root@${ip}:${remotePath}`;
+console.log(green('文件上传完成'));
+console.log('重启NG服务');
+await $`ssh ${host} "docker restart ${site}"`;
+console.log(green('部署完成'));
